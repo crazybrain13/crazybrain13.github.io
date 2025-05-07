95a532ce0fce9bb818f304345e65abc2324a0f1a
Phillip Johnson <phillip.aa.johnson@gmail.com>
2025-05-07 10:39:09 -0400

    All New Code
    Code is now running using React

---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+
+// CONSTANT
+// PascalCase
+// camelCase
+// snake_case
+
+function Message() {
+    const name = ""
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello World</h1>
+}
+
+export default Message
